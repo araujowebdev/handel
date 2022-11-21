f2656e22bc5f7a161d19ac067f91b0955a6d8827
Raphael Araujo <araujowebdev@gmail.com>
2022-11-21 15:27:27 -0300

    create header, footer and page home

--- app/public/wp-content/themes/handel/page-home.php
+++ app/public/wp-content/themes/handel/page-home.php
@@ -1,6 +1,10 @@
+<?php 
+// Template Name: Home
+?>
 <?php get_header(); ?>
   <?php if(have_posts()) { while(have_posts()) { the_post() ?>
     <h1><?php the_title(); ?></h1>
     <main><?php the_content(); ?></main>
+    Essa é a Home
   <?php }} ?>
   <?php get_footer(); ?>
