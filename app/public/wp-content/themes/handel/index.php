4613e3eecb4333539b94c842c7ad39eaec7bc2f8
Raphael Araujo <araujowebdev@gmail.com>
2022-11-29 10:54:32 -0300

    basic php class: variables and arrays

--- app/public/wp-content/themes/handel/index.php
+++ app/public/wp-content/themes/handel/index.php
@@ -8,7 +8,55 @@
 </head>
 <body>
 <?php
-  echo "Hello, Buddy!";
-?>  
+$nome = "Raphael";
+  echo get_the_title() . " " . $nome;
+?>
+<h1><?php echo $nome; ?></h1>
+<h1><?= $nome; ?></h1>
+
+<?php
+  $produtos = ['Camisetas', 'Bermudas', 'Casacos'];
+  echo $produtos[1];
+  print_r($produtos);
+  var_dump($produtos);
+  ?>
+  <pre>
+    <?= print_r($produtos) ?>
+    <?= var_dump($produtos) ?>
+  </pre>
+  <?php
+    $produto = [
+      'nome' => 'Camiseta Branca',
+      'preco' => 'R$ 59',
+      'img' => [
+        'src' => './img/camiseta_branca.jpg',
+        'alt' => 'Camiseta Branca',
+        ]
+      ];
+      var_dump($produto);
+  ?>
+  <h2><?= $produto['nome'] ?></h2>
+  <p><?= $produto['preco'] ?></p>
+  <img src="<?= $produto['img']['src'] ?>;" alt="<?= $produto['img']['alt'] ?>">
+
+  <br>
+  <?php
+    $categorias = ['Camisetas'];
+    var_dump($categorias);
+  ?>
+  <br>
+  <?php
+  unset($produto);
+  $categorias[] = 'Bermudas';
+  var_dump($categorias);
+  $produto['estoque'] = '10 itens';
+  var_dump($produto);
+  echo count($produto);
+  ?>
+  <br>
+  <?php
+  $produtos_chaves = array_keys($produtos);
+  print_r($produtos_chaves);
+  ?>
 </body>
 </html>
