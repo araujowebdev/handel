34adef34266c7edd22a49102de6c785363385e1d
Raphael Araujo <araujowebdev@gmail.com>
2022-11-30 08:38:51 -0300

    basic php class: functions

--- app/public/wp-content/themes/handel/index.php
+++ app/public/wp-content/themes/handel/index.php
@@ -7,75 +7,33 @@
   <title>Handel</title>
 </head>
 <body>
-<?php
-  // for($i = 0; $i <= 20; $i++) {
-  //   echo "\n" . $i;
-  // };
+  <?php
+    // $nome = get_bloginfo('version');
+    // echo $nome;
 
-  $produtos = [
-    [
-      'nome' => 'Camiseta Branca',
-      'preco' => 'R$ 59'
-    ],
-    [
-      'nome' => 'Bermuda Preta',
-      'preco' => 'R$ 79'
-    ]
-  ];
-
-  // foreach($produtos as $produto) {
-  //   echo '<h2>' . $produto['nome'] . '</h2>';
-  //   echo '<p>' . $produto['preco'] . '</p>';
-  // }
+    // function somar($a, $b) {
+    //   return $a + $b;
+    // }
+    // echo somar(5, 17);
+    
+    // function somar($a, $b) {
+    //   echo $a + $b;
+    // }
+    // somar(5, 17);
   ?>
-    <!-- <?php foreach($produtos as $produto) : ?>
-      <h2> <?= $produto['nome']; ?></h2>
-      <p> <?= $produto['preco']; ?></p>
-    <?php endforeach; ?> -->
   
-    <!-- <?php foreach($produtos as $produto) { ?>
-    <h2> <?= $produto['nome']; ?></h2>
-    <p> <?= $produto['preco']; ?></p>
-  <?php } ?> -->
-
-  <?php foreach($produtos as $key => $produto) { ?>
-    <?php foreach($produto as $key => $value) { ?>
-      <h1><?= $key . ' ' . $value; ?></h1>
-    <?php } ?>
+  <!-- <?php function formulario_contato() { ?>
+    <form action="">
+      <input type="text">
+      <input type="button" value="Enviar">
+    </form>
   <?php } ?>
 
-  <?php
-    $vitalicio = true;
-    if($vitalicio) {
-      echo 'Liberar';
-    }
-  ?>
+  <?php formulario_contato(); ?> -->
 
-<br><br>
+  <!-- <?php get_search_form(); ?> -->
 
-<?php
-  $categorias;
-  
-  if(empty($categorias)) {
-    echo 'Está vazio!';
-  }
-?>
-<br><br>
-<?php
-  $categorias = ['Bermuda'];
-  
-  if(!empty($categorias)) {
-    echo 'Não está vazio!';
-  }
-?>
-
-  <br><br>
-  <?php
-    $preco = 150;
-
-    $mensagem = $preco > 100 ? 'Caro' : 'Barato';
-
-    echo $mensagem;
-  ?>
+  <?php $teste = get_search_form(['echo' => false, 'aria_label' => 'MEU FORMULÁRIO']); ?>
+  <?php echo $teste; ?>
 </body>
 </html>
